391ea3673b7b18c4d1390c239e819cade6e95026
Marzieh mozaffari <121443046+Marzieh235@users.noreply.github.com>
2023-01-23 20:18:15 +0330

    show todos

--- src/Components/App.js
+++ src/Components/App.js
@@ -6,24 +6,34 @@ import 'bootstrap/dist/css/bootstrap.css'
 // Import Components
 import Header from './Header';
 import Formaddtodo from './Formaddtodo';
+import Todo from './Todo';
 
 class App extends Component {
     state = {
-        todos: [],
+        todos : [],
+        statusDone : false
     }
+
 
     addTodo(text) {
         this.setState(prevState => {
             return {
-                todos: [
+                todos : [
                     ...prevState.todos,
-                    { key: Date.now(), done: false, text }
-                ],
+                    { key : Date.now() , done : false , text }
+                ]
             }
         })
-    };
+    }
+
 
     render() {
+
+        let { todos , statusDone } = this.state;
+
+        let filterTodos = todos.filter(item => item.done === statusDone)
+
+
         return (
             <div className="App">
                 <Header />
@@ -32,54 +42,27 @@ class App extends Component {
                         <div className="container d-flex flex-column align-items-center">
                             <h1 className="jumbotron-heading">Welcome!</h1>
                             <p className="lead text-muted">To get started, add some items to your list:</p>
-                            <Formaddtodo add={this.addTodo.bind(this)}/>
+                            <Formaddtodo add={this.addTodo.bind(this)} />
                         </div>
                     </section>
                     <div className="todosList">
-                        <div className="container">
-                            <div className="d-flex flex-column align-items-center ">
-                                <nav className="col-6 mb-3">
-                                    <div className="nav nav-tabs" id="nav-tab" role="tablist">
-                                        <a className="nav-item nav-link active font-weight-bold" id="nav-home-tab">undone <span className="badge badge-secondary">9</span></a>
-                                        <a className="nav-item nav-link font-weight-bold" id="nav-profile-tab">done <span className="badge badge-success">9</span></a>
-                                    </div>
-                                </nav>
-                                <div className="col-6 mb-2">
-                                    <div className="d-flex justify-content-between align-items-center border rounded p-3">
-                                        <div>
-                                            hello roocket
+                            <div className="container">
+                                <div className="d-flex flex-column align-items-center ">
+                                    <nav className="col-6 mb-3">
+                                        <div className="nav nav-tabs" id="nav-tab" role="tablist">
+                                            <a className={`nav-item nav-link font-weight-bold ${ !statusDone ? 'active' : '' }`} id="nav-home-tab" onClick={() => this.setState({ statusDone : false })}>undone <span className="badge badge-secondary">{ todos.filter(item => item.done === false).length }</span></a>
+                                            <a className={`nav-item nav-link font-weight-bold ${ statusDone ? 'active' : '' }`} id="nav-profile-tab" onClick={() => this.setState({ statusDone : true })}>done <span className="badge badge-success">{ todos.filter(item => item.done === true).length}</span></a>
                                         </div>
-                                        <div>
-                                            <button type="button" className="btn btn-info btn-sm">edit</button>
-                                            <button type="button" className="btn btn-danger btn-sm ml-1">delete</button>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-6 mb-2">
-                                    <div className="d-flex justify-content-between align-items-center border rounded p-3">
-                                        <div>
-                                            hello roocket
-                                        </div>
-                                        <div>
-                                            <button type="button" className="btn btn-info btn-sm">edit</button>
-                                            <button type="button" className="btn btn-danger btn-sm ml-1">delete</button>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="col-6 mb-2">
-                                    <div className="d-flex justify-content-between align-items-center border rounded p-3">
-                                        <div>
-                                            hello roocket
-                                        </div>
-                                        <div>
-                                            <button type="button" className="btn btn-info btn-sm">edit</button>
-                                            <button type="button" className="btn btn-danger btn-sm ml-1">delete</button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                                    </nav>
+                                    {
+                                        filterTodos.length === 0
+                                            ?   <p>there isn`t any todos</p>
+                                            : filterTodos.map(item => <Todo key={item.key} text={item.text} />)
+                                    }
 
-                        </div>
+                                </div>
+                        
+                            </div>
                     </div>
                 </main>
             </div>
