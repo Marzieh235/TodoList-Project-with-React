f537ef7e03b1ba79c9230923a90a82cbd3f9b1bd
Marzieh mozaffari <121443046+Marzieh235@users.noreply.github.com>
2023-01-25 02:24:09 +0330

    Use context`s hook

--- src/Components/Todo/TodoList.js
+++ src/Components/Todo/TodoList.js
@@ -1,34 +1,30 @@
-import React , { useState } from 'react';
+import React, { useState, useContext } from 'react';
 
 import Todo from './Todo';
 import TodosContext from './../../Context/todos';
 
 function TodoList(props) {
 
-    const [ statusDone , setDone ] = useState(false);
+    const [statusDone, setDone] = useState(false);
+    const todosContext = useContext(TodosContext);
+    let { todos } = todosContext;
 
-    // let { todos } = props;
-
-    // let filterTodos = todos.filter(item => item.done === statusDone)
+    let filterTodos = todos.filter(item => item.done === statusDone)
 
     return (
-        <TodosContext.Consumer>
-            { context => (
-                    <>
-                        <nav className="col-6 mb-3">
-                            <div className="nav nav-tabs" id="nav-tab" role="tablist">
-                                <a className={`nav-item nav-link font-weight-bold ${ !statusDone ? 'active' : '' }`} id="nav-home-tab" onClick={() => setDone(false)}>undone <span className="badge badge-secondary">{ context.todos.filter(item => item.done === false).length }</span></a>
-                                <a className={`nav-item nav-link font-weight-bold ${ statusDone ? 'active' : '' }`} id="nav-profile-tab" onClick={() => setDone(true) }>done <span className="badge badge-success">{ context.todos.filter(item => item.done === true).length}</span></a>
-                            </div>
-                        </nav>
-                        {
-                            context.todos.filter(item => item.done === statusDone).length === 0
-                                ?   <p>there isn`t any todos</p>
-                                : context.todos.filter(item => item.done === statusDone).map(item => <Todo key={item.key} item={item} /> )
-                        }
-                    </>
-                )}
-        </TodosContext.Consumer>
+        <>
+            <nav className="col-6 mb-3">
+                <div className="nav nav-tabs" id="nav-tab" role="tablist">
+                    <a className={`nav-item nav-link font-weight-bold ${!statusDone ? 'active' : ''}`} id="nav-home-tab" onClick={() => setDone(false)}>undone <span className="badge badge-secondary">{todos.filter(item => item.done === false).length}</span></a>
+                    <a className={`nav-item nav-link font-weight-bold ${statusDone ? 'active' : ''}`} id="nav-profile-tab" onClick={() => setDone(true)}>done <span className="badge badge-success">{todos.filter(item => item.done === true).length}</span></a>
+                </div>
+            </nav>
+            {
+                filterTodos.length === 0
+                    ? <p>there isn`t any todos</p>
+                    : filterTodos.filter(item => item.done === statusDone).map(item => <Todo key={item.key} item={item} />)
+            }
+        </>
     )
 }
 
