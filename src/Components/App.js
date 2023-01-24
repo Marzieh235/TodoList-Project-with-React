3341ee7ed0c7285eb6da643f0ecb3981c4011091
Marzieh mozaffari <121443046+Marzieh235@users.noreply.github.com>
2023-01-24 10:43:49 +0330

    edit Todo

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,49 +1,66 @@
-import React, { Component } from 'react';
+import React , { Component } from 'react';
 
 import 'bootstrap/dist/css/bootstrap.css'
 
 
 // Import Components
 import Header from './Header';
-import Formaddtodo from './Formaddtodo';
+import FormAddTodo from './FormAddTodo';
 import Todo from './Todo';
 
 class App extends Component {
     state = {
-        todos: [],
-        statusDone: false
+        todos : [],
+        statusDone : false
     }
 
 
     addTodo(text) {
         this.setState(prevState => {
             return {
-                todos: [
+                todos : [
                     ...prevState.todos,
-                    { key: Date.now(), done: false, text }
+                    { key : Date.now() , done : false , text }
                 ]
             }
         })
     }
 
-    DeleteTodo(key) {
+
+    deleteTodo(key) {
         this.setState(prevState => {
             return {
-                todos: prevState.todos.filter(item => item.key !== key)
+                todos : prevState.todos.filter(item => item.key !== key)
             }
         })
     }
 
-    toggleTodo(key) {
-        let { todos } = this.state
+    editTodo(key , text) {
+        let { todos } = this.state;
+
         let item = todos.find(item => item.key === key);
-        item.done = ! item.done;
+        item.text = text ;
 
-        let newTodos = todos.filter(item => item.key !== key);
-
+        let newTodos = todos.filter(item => item.key !== key)
 
         this.setState({
-            todos: [
+            todos : [
+                ...newTodos,
+                item
+            ]
+        })
+    }
+
+    toggleTodo(key) {
+        let { todos } = this.state;
+
+        let item = todos.find(item => item.key === key);
+        item.done = ! item.done ;
+
+        let newTodos = todos.filter(item => item.key !== key)
+
+        this.setState({
+            todos : [
                 ...newTodos,
                 item
             ]
@@ -52,7 +69,7 @@ class App extends Component {
 
     render() {
 
-        let { todos, statusDone } = this.state;
+        let { todos , statusDone } = this.state;
 
         let filterTodos = todos.filter(item => item.done === statusDone)
 
@@ -65,32 +82,34 @@ class App extends Component {
                         <div className="container d-flex flex-column align-items-center">
                             <h1 className="jumbotron-heading">Welcome!</h1>
                             <p className="lead text-muted">To get started, add some items to your list:</p>
-                            <Formaddtodo add={this.addTodo.bind(this)} />
+                            <FormAddTodo add={this.addTodo.bind(this)} />
                         </div>
                     </section>
                     <div className="todosList">
-                        <div className="container">
-                            <div className="d-flex flex-column align-items-center ">
-                                <nav className="col-6 mb-3">
-                                    <div className="nav nav-tabs" id="nav-tab" role="tablist">
-                                        <a className={`nav-item nav-link font-weight-bold ${!statusDone ? 'active' : ''}`} id="nav-home-tab" onClick={() => this.setState({ statusDone: false })}>undone <span className="badge badge-secondary">{todos.filter(item => item.done === false).length}</span></a>
-                                        <a className={`nav-item nav-link font-weight-bold ${statusDone ? 'active' : ''}`} id="nav-profile-tab" onClick={() => this.setState({ statusDone: true })}>done <span className="badge badge-success">{todos.filter(item => item.done === true).length}</span></a>
-                                    </div>
-                                </nav>
-                                {
-                                    filterTodos.length === 0
-                                        ? <p>there isn`t any todos</p>
-                                        : filterTodos.map(item => <Todo
-                                            key={item.key}
-                                            item={item}
-                                            delete={this.DeleteTodo.bind(this)}
-                                            done={this.toggleTodo.bind(this)}
-                                        />)
-                                }
+                            <div className="container">
+                                <div className="d-flex flex-column align-items-center ">
+                                    <nav className="col-6 mb-3">
+                                        <div className="nav nav-tabs" id="nav-tab" role="tablist">
+                                            <a className={`nav-item nav-link font-weight-bold ${ !statusDone ? 'active' : '' }`} id="nav-home-tab" onClick={() => this.setState({ statusDone : false })}>undone <span className="badge badge-secondary">{ todos.filter(item => item.done === false).length }</span></a>
+                                            <a className={`nav-item nav-link font-weight-bold ${ statusDone ? 'active' : '' }`} id="nav-profile-tab" onClick={() => this.setState({ statusDone : true })}>done <span className="badge badge-success">{ todos.filter(item => item.done === true).length}</span></a>
+                                        </div>
+                                    </nav>
+                                    {
+                                        filterTodos.length === 0
+                                            ?   <p>there isn`t any todos</p>
+                                            : filterTodos.map(item => <Todo 
+                                                                            key={item.key} 
+                                                                            item={item} 
+                                                                            delete={this.deleteTodo.bind(this)}
+                                                                            done={this.toggleTodo.bind(this)} 
+                                                                            edit={this.editTodo.bind(this)}
+                                                                        />
+                                                                    )
+                                    }
 
+                                </div>
+                        
                             </div>
-
-                        </div>
                     </div>
                 </main>
             </div>
