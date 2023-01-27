ac9574dca13a104e22f230f32702849ff350afb0
Marzieh mozaffari <121443046+Marzieh235@users.noreply.github.com>
2023-01-27 21:28:00 +0330

    switch between pages

--- src/Components/Layouts/Header.js
+++ src/Components/Layouts/Header.js
@@ -1,7 +1,7 @@
-import React , { useContext } from 'react';
+import React, { useContext } from 'react';
 import AuthContext from '../../Context/auth';
 import TodosContext from '../../Context/todos';
-
+import { Link } from 'react-router-dom';
 
 function Header() {
 
@@ -9,18 +9,34 @@ function Header() {
     const authContext = useContext(AuthContext);
 
 
-    let doLogin = () => authContext.dispatch({ type : 'login_user' });
-    let doLogout = () => authContext.dispatch({ type : 'logout_user'});
+    let doLogin = () => authContext.dispatch({ type: 'login_user' });
+    let doLogout = () => authContext.dispatch({ type: 'logout_user' });
 
     return (
         <header>
             <div className="navbar navbar-dark bg-dark shadow-sm">
                 <div className="container d-flex justify-content-between">
-                    <a href="#" className="navbar-brand d-flex align-items-center">
-                        <strong>Todo App</strong>
-                    </a>
+
+
+
+                    <div className='d-flex'>
+                        <a href="#" className="navbar-brand d-flex align-items-center">
+                            <strong>Todo App</strong>
+                        </a>
+                        <ul className='nav'>
+                            <li className='nav-item'>
+                                <Link className='nav-link active text-white' to="/">Home</Link>
+                            </li>
+                            <li className='nav-item'>
+                                <Link className='nav-link text-white'to="/about">About</Link>
+                            </li>
+                            <li className='nav-item'>
+                                <Link className='nav-link text-white' to="/contact-us">ContactUs</Link>
+                            </li>
+                        </ul>
+                    </div>
                     {
-                        ! authContext.authenticated
+                        !authContext.authenticated
                             ? <button className="btn btn-sm btn-success" onClick={doLogin}>login</button>
                             : <button className="btn btn-sm btn-danger" onClick={doLogout}>logout</button>
                     }
